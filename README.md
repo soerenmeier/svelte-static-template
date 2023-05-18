3313514b66b882951317257a33678d1f59b61dad
Sören Meier <soeren@s-me.ch>
2023-05-18 21:22:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## dev setup
+```bash
+npm install
+npm run dev
+```
+
+## Prod setup
+```bash
+npm run build
+```
+
+The final files are in `dist`.
---
+++ src/app.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Navigation from './layout/navigation.svelte';
+	import Footer from './layout/footer.svelte';
+
+	export let page;
+</script>
+
+<div id="app">
+	<header>
+		<Navigation />
+	</header>
+
+	<main>
+		<svelte:component this={page.comp} {...page.props} />
+	</main>
+
+
+	<footer>
+		<Footer />
+	</footer>
+</div>
